ef1764938aad8f449559292532099e942b89856a
YOUR NAME <YOUR@EMAIL.com>
2021-06-11 15:07:46 -0600

    YES

--- js/main.js
+++ js/main.js
@@ -1,4 +1,98 @@
-const root = document.getElementById("app");
+document.getElementById('bub1').onclick = function(event) {
+    document.getElementById('bub1').style.display = "none";
+}
+
+document.getElementById('bub2').onclick = function(event) {
+    document.getElementById('bub2').style.display = "none";
+}
+
+document.getElementById('bub3').onclick = function(event) {
+    document.getElementById('bub3').style.display = "none";
+}
+
+document.getElementById('bub4').onclick = function(event) {
+    document.getElementById('bub4').style.display = "none";
+}
+
+document.getElementById('bub5').onclick = function(event) {
+    document.getElementById('bub5').style.display = "none";
+}
+
+document.getElementById('bub6').onclick = function(event) {
+    document.getElementById('bub6').style.display = "none";
+}
+
+document.getElementById('bub7').onclick = function(event) {
+    document.getElementById('bub7').style.display = "none";
+}
+
+document.getElementById('bub8').onclick = function(event) {
+    document.getElementById('bub8').style.display = "none";
+}
+
+document.getElementById('bub9').onclick = function(event) {
+    document.getElementById('bub9').style.display = "none";
+}
+
+document.getElementById('bub10').onclick = function(event) {
+    document.getElementById('bub10').style.display = "none";
+}
+
+
+/*var bub1 = document.getElementsByClassName("x1");
+var bub2 = document.getElementsByClassName("x2");
+var bub3 = document.getElementsByClassName("x3");
+var bub4 = document.getElementsByClassName("x4");
+var bub5 = document.getElementsByClassName("x5");
+var bub6 = document.getElementsByClassName("x6");
+var bub7 = document.getElementsByClassName("x7");
+var bub8 = document.getElementsByClassName("x8");
+var bub9 = document.getElementsByClassName("x9");
+var bub10 = document.getElementsByClassName("x10");
+
+
+bub1.addEventListener("click", function (event){
+    bub1.style.display = "none";
+});
+
+bub2.addEventListener("click", function (event){
+    bub2.style.display = "none";
+});
+
+bub3.addEventListener("click", function (event){
+    bub3.style.display = "none";
+});
+
+bub4.addEventListener("click", function (event){
+    bub4.style.display = "none";
+});
+
+bub5.addEventListener("click", function (event){
+    bub5.style.display = "none";
+});
+
+bub6.addEventListener("click", function (event){
+    bub6.style.display = "none";
+});
+
+bub7.addEventListener("click", function (event){
+    bub7.style.display = "none";
+});
+
+bub8.addEventListener("click", function (event){
+    bub8.style.display = "none";
+});
+
+bub9.addEventListener("click", function (event){
+    bub9.style.display = "none";
+});
+
+bub10.addEventListener("click", function (event){
+    bub10.style.display = "none";
+});
+*/
+
+/*const root = document.getElementById("app");
 //getting "bubble" from HTML
 
 
@@ -65,3 +159,4 @@ bubbleEnd(removingTime = 0){
 setInterval(function () {
     new Bubble
 }, 1000)
+*/
