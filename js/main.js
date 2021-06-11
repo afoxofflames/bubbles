9d9cb8e2c628fc3fca93064fdeb9469a7574c47c
YOUR NAME <YOUR@EMAIL.com>
2021-06-11 15:36:01 -0600

    YAY

--- js/main.js
+++ js/main.js
@@ -38,125 +38,42 @@ document.getElementById('bub10').onclick = function(event) {
     document.getElementById('bub10').style.display = "none";
 }
 
-
-/*var bub1 = document.getElementsByClassName("x1");
-var bub2 = document.getElementsByClassName("x2");
-var bub3 = document.getElementsByClassName("x3");
-var bub4 = document.getElementsByClassName("x4");
-var bub5 = document.getElementsByClassName("x5");
-var bub6 = document.getElementsByClassName("x6");
-var bub7 = document.getElementsByClassName("x7");
-var bub8 = document.getElementsByClassName("x8");
-var bub9 = document.getElementsByClassName("x9");
-var bub10 = document.getElementsByClassName("x10");
-
-
-bub1.addEventListener("click", function (event){
-    bub1.style.display = "none";
-});
-
-bub2.addEventListener("click", function (event){
-    bub2.style.display = "none";
-});
-
-bub3.addEventListener("click", function (event){
-    bub3.style.display = "none";
-});
-
-bub4.addEventListener("click", function (event){
-    bub4.style.display = "none";
-});
-
-bub5.addEventListener("click", function (event){
-    bub5.style.display = "none";
-});
-
-bub6.addEventListener("click", function (event){
-    bub6.style.display = "none";
-});
-
-bub7.addEventListener("click", function (event){
-    bub7.style.display = "none";
-});
-
-bub8.addEventListener("click", function (event){
-    bub8.style.display = "none";
-});
-
-bub9.addEventListener("click", function (event){
-    bub9.style.display = "none";
-});
-
-bub10.addEventListener("click", function (event){
-    bub10.style.display = "none";
-});
-*/
-
-/*const root = document.getElementById("app");
-//getting "bubble" from HTML
-
-
-console.log(innerWidth,innerHeight);
-
-class Bubble {
-
-constructor() {
-    this.bubbleSpan = undefined;
-    this.handleNewBubble();
-
-    this.posY = this.getRandomNumber(innerHeight - 20, 20);
-    this.posX = this.getRandomNumber(innerHeight -20, 20);
-
-    this.bubbleSpan.style.top = this.posY + "px";
-    this.bubbleSpan.style.left = this.posX + "px";
-
-    this.fontSize = this.getRandomNumber(20, 50);
-
-    this.bubbleEnd.call(this.bubbleSpan, this.getRandomNumber(10000,6000));
+document.getElementById('bub11').onclick = function(event) {
+    document.getElementById('bub11').style.display = "none";
 }
 
-handleNewBubble () {
-    this.bubbleSpan = document.createElement("span");
-    this.bubbleSpan.classList.add("bubble");
-    const newContent = document.createTextNode("Bubbles")
-    this.bubbleSpan.appendChild(newContent);
-    this.handlePosition();
-
-    this.bubbleSpan.addEventListener("click",this.bubbleEnd);
+document.getElementById('bub12').onclick = function(event) {
+    document.getElementById('bub12').style.display = "none";
 }
 
-handlePosition() {
-    const randomY = this.getRandomNumber (60, -60);
-    const randomX = this.getRandomNumber (60, -60);
-
-    this.posY = this.getRandomNumber (innerHeight - 20, 20);
-    this.posX = this.getRandomNumber (innerWidth - 20, 20);
-
-    this.bubbleSpan.style.top = this.posY + "px";
-    this.bubbleSpan.style.left = this.posX + "px";
-
-    const randomSec = this.getRandomNumber(4000, 200);
-
-    setTimeout(this.handlePosition.bind(this),randomSec);
+document.getElementById('bub13').onclick = function(event) {
+    document.getElementById('bub13').style.display = "none";
 }
 
-getRandomNumber(max, min) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-bubbleEnd(removingTime = 0){
-    setTimeout(() => {
-        this.classList.add('bubble--burst');
-    }, removingTime === 0 ? removingTime : removingTime - 100);
-
-    setTimeout(() => {
-        this.remove();
-        bubbles.push(new Bubble)
-    }, removingTime)
-}
+document.getElementById('bub14').onclick = function(event) {
+    document.getElementById('bub14').style.display = "none";
 }
 
-setInterval(function () {
-    new Bubble
-}, 1000)
-*/
+document.getElementById('bub15').onclick = function(event) {
+    document.getElementById('bub15').style.display = "none";
+}
+
+document.getElementById('bub16').onclick = function(event) {
+    document.getElementById('bub16').style.display = "none";
+}
+
+document.getElementById('bub17').onclick = function(event) {
+    document.getElementById('bub17').style.display = "none";
+}
+
+document.getElementById('bub18').onclick = function(event) {
+    document.getElementById('bub18').style.display = "none";
+}
+
+document.getElementById('bub19').onclick = function(event) {
+    document.getElementById('bub19').style.display = "none";
+}
+
+document.getElementById('bub20').onclick = function(event) {
+    document.getElementById('bub20').style.display = "none";
+}
